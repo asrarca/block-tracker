0c301f8b8c95d310e38945f3921d255e2eb205ae
asrarca <asrar.ca@gmail.com>
2025-07-28 19:07:45 -0400

    Added support for multi-chain.

--- app/config.js
+++ app/config.js
@@ -1,6 +1,16 @@
 const config = {
   ETHERSCAN_API_KEY: 'DV5RFA6I7VYDJA4XR3DGZSD1F625UQ8Q7Y',
   ETHERSCAN_API_URL: 'https://api.etherscan.io/v2/api',
+  CHAINS: {
+    1: {
+      name: 'Ethereum',
+      unit: 'ETH',
+    },
+    56: {
+      name: 'Binance Chain',
+      unit: 'BNB',
+    },
+  },
 };
 
 export default config;
