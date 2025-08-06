a865be5d4d68fbbd2c692b82d7d8b26a3321ca44
asrarca <asrar.ca@gmail.com>
2025-08-06 11:35:32 -0400

    Added more valid image domains.

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['placehold.co', 'tokens.1inch.io', 'raw.githubusercontent.com'],
-  },  
+    domains: [
+      'placehold.co',
+      'arbitrum.foundation',
+      'assets.coingecko.com',
+      'basescan.org',
+      'coin-images.coingecko.com',
+      'data.1inch.io',
+      'dynamic-assets.coinbase.com',
+      'ethereum-optimism.github.io',
+      'minerva.digital',
+      'raw.githubusercontent.com',
+      's2.coinmarketcap.com',
+      'snowtrace.io',
+      'tokens-data.1inch.io',
+      'tokens.1inch.io',
+      'tokens.pancakeswap.finance',
+    ],
+  },
 };
 
 export default nextConfig;
